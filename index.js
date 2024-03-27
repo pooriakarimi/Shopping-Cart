c6d6a3ca88af9437d1a05f6ea3028c130a8273dd
Pooria Karimi <pooria.karimi.55@gmail.com>
2024-03-28 00:27:32 +0330

    html added

---
+++ index.js
@@ -0,0 +1,4 @@
+//*Shopping Cart JS Code
+//todo Tasks: Dynamic buttons : when you click button, item get added to cart
+//todo when you click on your final cart, a modal shows up and items can be seen with details
+//todo datas be saved on localStorage  or sessionStorage. If not available show empty cart

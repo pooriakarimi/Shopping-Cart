6b30743354efa1d3568ebd6b9bac50ebe055289c
Pooria Karimi <pooria.karimi.55@gmail.com>
2024-03-31 00:47:28 +0330

    JS started

--- index.js
+++ index.js
@@ -2,3 +2,42 @@
 //todo Tasks: Dynamic buttons : when you click button, item get added to cart
 //todo when you click on your final cart, a modal shows up and items can be seen with details
 //todo datas be saved on localStorage  or sessionStorage. If not available show empty cart
+
+// Selectors
+
+const backdrop = document.querySelector(".backdrop");
+const cart = document.querySelector(".cart");
+const cartBtn = document.querySelector(".cart__btn");
+const cartExit = document.querySelector(".close--btn");
+// Cart Button Function
+cartBtn.addEventListener("click", cartBtnFunc);
+cartExit.addEventListener("click", cartExitFunc);
+
+function cartBtnFunc() {
+  backdrop.style.display = "block";
+  cart.style.display = "flex";
+}
+function cartExitFunc() {
+  backdrop.style.display = "none";
+  cart.style.display = "none";
+}
+
+//todo 1.get products
+//todo 2. display products
+//todo 3. storage
+
+import { productsData } from "/products.js";
+
+class Products {
+  //get from api  end point !
+  getProducts() {
+    return productsData;
+  }
+}
+class UI {}
+
+class storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded!");
+});
---
+++ products.js
@@ -0,0 +1,51 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Air Max 90 flyease",
+    price: 79.99,
+    imageUrl: "/Assets/Images/air-max-90-flyease.png",
+  },
+  {
+    id: 2,
+    title: "Running Air White",
+    price: 85.99,
+    imageUrl: "/Assets/Images/Frame 611.png",
+  },
+  {
+    id: 3,
+    title: "Running Flat Red",
+    price: 59.99,
+    imageUrl: "/Assets/Images/Frame 612.png",
+  },
+  {
+    id: 4,
+    title: "Air Max 02 Green",
+    price: 79.99,
+    imageUrl: "./Assets/Images/pngimg.com - running_shoes_PNG5816.png",
+  },
+  {
+    id: 5,
+    title: "Running Black Gold",
+    price: 67.99,
+    imageUrl: "./Assets/Images/PikPng.com_nike-shoes-png_1861350.png",
+  },
+  {
+    id: 6,
+    title: "Air Jordan One",
+    price: 99.99,
+    imageUrl:
+      "./Assets/Images/kisspng-sneakers-air-jordan-nike-shoe-off-white-5af7162bd2fa54.4472027515261425078642.png",
+  },
+  {
+    id: 7,
+    title: "Lebron Miami Edition",
+    price: 56.99,
+    imageUrl: "./Assets/Images/PikPng.com_nike-shoes-png_1861322.png",
+  },
+  {
+    id: 8,
+    title: "Air Max 22 Green",
+    price: 89.99,
+    imageUrl: "./Assets/Images/PikPng.com_nike-png_3010824.png",
+  },
+];
